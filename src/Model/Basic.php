ec47f389fadb539c92bfd303fbedaf520c33beec
skybedy <skybedy@gmail.com>
2021-12-30 10:28:39 +0100

    dodelal jsem funkci nextRaces

--- src/Model/Basic.php
+++ src/Model/Basic.php
@@ -4,11 +4,20 @@ namespace App\Model;
 
 class Basic {
 
-    public $currentYear;
     public $sqlZavody;
 
     public function sqlShortcuts($raceYear){
         $this->sqlZavody = 'zavody_'.$raceYear;
+    }
+
+    protected function currentYear()
+    {
+        return date("Y");
+    }
+
+    protected function nextYear()
+    {
+        return date("Y");
     }
 
 
--- src/Model/Home.php
+++ src/Model/Home.php
@@ -2,8 +2,6 @@
 
 namespace App\Model;
 
-
-
 /**
  * Model pro výběr nejbližších závodů a posledních výsledků
  * @package App\Model
@@ -11,24 +9,20 @@ namespace App\Model;
 class Home extends Basic
 {
 
-
-    
-    
     public function  nextRaces($conn)
     {
-      //$this->sqlShortcuts(date("Y"));
-      
-       // return $this->sqlZavody;
-
-       $year = date("Y");
-     
-     
-        $sql = "SELECT zv.id_zavodu,zv.nazev_zavodu,zv.kod_zavodu,DATE_FORMAT(zv.datum_zavodu,'%e.%c.%Y') AS datum,tz.icon,zv.web FROM zavody_{$year} zv, typ_zavodu tz WHERE zv.datum_zavodu  > CURDATE() AND zv.typ_zavodu = tz.id_typ_zavodu ORDER BY zv.datum_zavodu ASC LIMIT 0,4";
-        $stmt =  $conn->prepare($sql);
-        $stmt->bindValue(1,10);
-        $resultSet = $stmt->executeQuery();
-        $dbData = $resultSet->fetchAllAssociative();
+        $sql = "SELECT zv.id_zavodu,zv.nazev_zavodu,zv.kod_zavodu,DATE_FORMAT(zv.datum_zavodu,'%e.%c.%Y') AS datum,tz.icon,zv.web FROM zavody_{$this->currentYear()} zv, typ_zavodu tz WHERE zv.datum_zavodu  > CURDATE() AND zv.typ_zavodu = tz.id_typ_zavodu ORDER BY zv.datum_zavodu ASC LIMIT 0,4";
+        $dbData = $conn->fetchAllAssociative($sql);
+        if(count($dbData) < 4)
+        {
+            $sql1 = "SELECT zv.id_zavodu,zv.nazev_zavodu,zv.kod_zavodu,DATE_FORMAT(zv.datum_zavodu,'%e.%c.%Y') AS datum,tz.icon,zv.web FROM zavody_{$this->nextYear()} zv, typ_zavodu tz WHERE zv.datum_zavodu  > CURDATE() AND zv.typ_zavodu = tz.id_typ_zavodu ORDER BY zv.datum_zavodu ASC LIMIT 0,4";
+            $dbData1 = $conn->fetchAllAssociative($sql1);
+            foreach($dbData1 as $arr)
+            {
+                $dbData[] = $arr;
+            }
+        }
         return $dbData;
-        
     }
 }
+
