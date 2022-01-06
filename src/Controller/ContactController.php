775008a322605a5efa5380528deea5055c0ad222
skybedy <skybedy@gmail.com>
2022-01-06 12:04:11 +0100

    dodelani controlleru mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,18 +6,35 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request)
+    public function index(Request $request,MailerInterface $mailer)
     {
 
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
         
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+            $email = (new Email())
+            ->from($contactFormData['email'])
+            ->to('info@timechip.cz')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Zpráva z webového formuláře')
+            ->text($contactFormData['zprava']);
+            $mailer->send($email);
+            $this->addFlash('success', 'Message was send');
+            return $this->redirectToRoute('contact');
+        }
         
         
         
